167906c37cd54646108df06edaa5f8d83710840a
antoinedhanis98 <antoine.dhanis@student.umons.ac.be>
2021-03-29 16:57:48 +0200

    Creation des deux scenes

--- js/App.js
+++ js/App.js
@@ -1,41 +1,146 @@
-const container = document.body // Document.body permet d'accèder à tous les éléments de la page web
-let taille = document.getElementById('taille').offsetHeight;
+const container = document.body
+const tooltip = document.querySelector('.tooltipVV')
+let tooltipActive = false
 
-//   CREATION DE LA SCENE, CAMERA, RENDU  //
+let taille = document.getElementById('taille').offsetHeight;
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight-taille, 0.1, 1000 );
+camera.aspect = window.innerWidth/(window.innerHeight-taille);
+camera.updateProjectionMatrix()
+
+class Scene{
+    constructor(image, camera) {
+        this.image = image
+        this.points = []
+        this.sprites =[]
+        this.scene = null
+        this.camera = camera
+    }
+
+    createScene(scene){
+        this.scene = scene
+        scene.background = new THREE.Color('white')
+        const geometry = new THREE.SphereGeometry( 50, 32, 32 );
+        const texture = new THREE.TextureLoader().load(this.image);
+        texture.wrapS = THREE.RepeatWrapping
+        texture.repeat.x = -1
+        const material = new THREE.MeshBasicMaterial( {
+            map : texture,
+            side : THREE.DoubleSide
+        } );
+        material.transparent = true
+        this.sphere = new THREE.Mesh( geometry, material );
+        this.scene.add( this.sphere );
+        this.points.forEach(this.addTooltip.bind(this))
+    }
+    addPoints (point){
+        this.points.push(point)
+
+    }
+    addTooltip (point) {
+        let map = new THREE.TextureLoader().load( 'img/VirtualTour/info.png' );
+        let spritematerial = new THREE.SpriteMaterial( { map: map } );
+        let sprite = new THREE.Sprite( spritematerial );
+        sprite.name = point.name
+        sprite.position.copy(point.position.clone().normalize().multiplyScalar(30))
+        sprite.scale.multiplyScalar(1.5);
+        this.scene.add( sprite );
+        this.sprites.push(sprite)
+        sprite.onclick =() => {
+            this.destroy()
+            point.scene.createScene(scene)
+            point.scene.appear()
+        }
+    }
+
+    destroy() {
+        gsap.to(this.camera,0.5, {
+            zoom:3,
+            onUpdate: () => {
+                this.camera.updateProjectionMatrix()
+            }
+        })
+        gsap.to(this.sphere.material,1, {
+            opacity:0,
+            onComplete: () => {
+                this.scene.remove(this.sphere)
+            }
+        })
+        this.sprites.forEach((sprite)=>{
+            gsap.to(sprite.scale,1, {
+                x:0,
+                y:0,
+                z:0,
+                onComplete: () => {
+                    this.scene.remove(sprite)
+                }
+            })
+        })
+    }
+
+    appear(){
+        this.sphere.material.opacity =0
+        gsap.to(this.camera,0.5, {
+            zoom:1,
+            onUpdate: () => {
+                this.camera.updateProjectionMatrix()
+            }
+        }).delay(0.5)
+        gsap.to(this.sphere.material,1, {
+            opacity:1
+        })
+        this.sprites.forEach((sprite)=>{
+            sprite.scale.set(0,0,0)
+            gsap.to(sprite.scale,1, {
+                x:2,
+                y:2,
+                z:2
+            })
+        })
+    }
+}
+
+
+
+//   RENDU ET PARAMETRES CONTROLES //
 const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight-taille);
+renderer.setSize(window.innerWidth, (window.innerHeight-taille))
 document.body.appendChild(renderer.domElement)
 
-//   CREATION SCENE  //
-const scene = new THREE.Scene();
+let s = new Scene('img/virtualTour/cour_Exterieure.jpg', camera)
+let s2 = new Scene('img/virtualTour/hallentree.jpg', camera)
+s.createScene(scene)
+s.appear()
+s.addPoints(
+    {position: new THREE.Vector3(48.530288176522504,  0.38881285381676156, -11.366902025238447),
+        name: 'Entrée',
+        scene: s2
+    })
+s2.addPoints({
+    position: new THREE.Vector3(-28.558965443557415,  0.7382878215597668, 40.88628571718127),
+    name: 'Sortie',
+    scene: s
+})
+s.addTooltip({position: new THREE.Vector3(48.530288176522504,  0.38881285381676156, -11.366902025238447),
+        name: 'Entrée',
+        scene: s2
+    }
+)/*
+s2.addTooltip({position: new THREE.Vector3(-28.558965443557415,  0.7382878215597668, 40.88628571718127),
+        name: 'Sortie',
+        scene: s
+    }
+)*/
 
-//   CREATION CAMERA  //
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / (window.innerHeight-taille) , 0.1, 200 );
-camera.position.set(-1,0,0) ;//vu qu'on a inversé la texture il faut inverser la caméra pour regarder au début
 
-//   Contrôle de la rotation de la caméra  //
+container.addEventListener('mousemove',onMouseMove)
 
 const controls = new THREE.OrbitControls(camera,renderer.domElement); //permet à la caméra de trourner sur son orbite
+controls.rotateSpeed = 0.2;
 controls.enableZoom = false;
 controls.enablePan = false;//impossible de bouger la caméra en x,y,z
 controls.update(); //doit être appelé après transfo de la caméra
-
-
-//   CREATION DE LA SPHERE //
-
-const geometry = new THREE.SphereGeometry( 50, 32, 32 );
-const texture = new THREE.TextureLoader().load('img/virtualTour/hallentree.jpg')
-texture.repeat.x = -1 //inverse la texture afin que l'image soit droite et non inversée
-texture.wrapS = THREE.RepeatWrapping // à utiliser avec la ligne du haut
-const material = new THREE.MeshBasicMaterial({
-    map : texture,
-    side : THREE.DoubleSide
-})
-const sphere = new THREE.Mesh( geometry, material );
-scene.add( sphere );
-renderer.render( scene, camera );
-
-//   ROTATION SPHERE  //
+camera.position.set(0,0,-1);
 function animate() {
     requestAnimationFrame( animate );
     controls.update()
@@ -49,21 +154,8 @@ function onReSize() {
     renderer.setSize(window.innerWidth, (window.innerHeight-taille))
     camera.aspect = window.innerWidth/(window.innerHeight-taille);
     camera.updateProjectionMatrix()
-
 }
 window.addEventListener('resize',onReSize)
-
-//  AJOUT D'IMAGES INFO //
-function addTooltip (position,name){
-    let spriteMap = new THREE.TextureLoader().load('img/virtualTour/info.png')
-    let spriteMaterial = new THREE.SpriteMaterial( { map:spriteMap } )
-    let sprite = new THREE.Sprite(spriteMaterial) //sprite regarde toujours la caméra
-
-    sprite.name = name
-    sprite.position.copy(position.clone().normalize().multiplyScalar(20))
-    scene.add(sprite)
-}
-addTooltip(new THREE.Vector3(49.75463396722639 ,-4.8919163713478016 ,-0.05117854755698681),'Entrée')
 
 //  MANIERE DE TROUVER COORD DE LA SPRITE //
 const rayCaster = new THREE.Raycaster()
@@ -73,11 +165,16 @@ function onClick (e) {
         (e.clientX / window.innerWidth) * 2 - 1,
         -(e.clientY / (window.innerHeight+taille)) * 2 + 1
     )
+
     rayCaster.setFromCamera(mouse, camera)
-    let intersects = rayCaster.intersectObject(sphere)
-    if (intersects.length > 0) {
+    let intersects = rayCaster.intersectObjects(scene.children)
+    intersects.forEach(function(intersect){
         console.log('Sphère touchée au point : ', intersects[0].point)
-    }
+        if(intersect.object.type=='Sprite'){
+            intersect.object.onclick()
+        }
+    })
+
 
 }
 
@@ -88,14 +185,23 @@ function onMouseMove(e){
         (e.clientX / window.innerWidth) * 2 - 1,
         -(e.clientY / (window.innerHeight+taille)) * 2 + 1
     )
-    rayCaster.setFromCamera(mouse, camera)
-    let intersects =rayCaster.intersectObjects(scene.children)
 
+    rayCaster.setFromCamera(mouse, camera)
+    let foundSprite = false
+    let intersects = rayCaster.intersectObjects(scene.children)
     intersects.forEach(function(intersect){
-        if (intersect.object.type ==='Sprite'){
-            console.log(intersect.object.name)
+        if(intersect.object.type=='Sprite'){
+            let p = intersect.object.position.clone().project(camera)
+            tooltip.style.top = ((-1*p.y+1)*window.innerHeight/2) + 'px'
+            tooltip.style.left = ((p.x+1)*window.innerWidth/2)+'px'
+            tooltip.classList.add('is-active')
+            tooltip.innerHTML = intersect.object.name
+            tooltipActive = true
+            foundSprite = true
         }
     })
-}
+    if ((foundSprite == false && tooltipActive)){
+        tooltip.classList.remove('is-active')
+    }
 
-container.addEventListener('mousemove',onMouseMove)
+}
